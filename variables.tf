1976e4da934bd83471f1f1a0d1056255d8b72278
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-09-27 19:44:49 +0200

    add initial set of variables

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "region" {
+  description = "The region to deploy resources to"
+  default     = "northeurope"
+  type        = string
+}
+
+variable "container_name" {
+  description = "The name of the Azure storage container to deploy"
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to apply to all resources associated with the remote storage"
+  type        = map(string)
+  default     = {}
+}
+
+# variable "force_destroy" {
+#   description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error"
+#   default     = false
+#   type        = bool
+# }
+
+# variable "enable_versioning" {
+#   description = "A boolean that indicates all objects should be versioned"
+#   default     = true
+#   type        = bool
+# }
+
+
+
+# variable "block_public_access" {
+#   description = "Whether or not to block public access to the bucket"
+#   default     = true
+#   type        = bool
+# }
